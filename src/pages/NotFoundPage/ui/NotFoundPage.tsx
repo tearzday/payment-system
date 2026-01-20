ef57b9fb0c64ef105526e4232c9e711b262c039d
tearzday <biverdaniil@gmail.com>
2026-01-20 15:31:11 +0300

    style: add for NotFoundPage

--- src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,3 +1,16 @@
+import { Button } from '@/shared/ui';
+import cls from './NotFoundPage.module.css';
+import { useNavigate } from 'react-router';
+
 export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <div className={cls.container}>
+      <h1>404 - Страница не найдена</h1>
+      <img className={cls.image} src="https://i.gifer.com/1FA.gif" alt="Monkey" />
+      <Button onClick={() => navigate('/')} theme="btn--accent">
+        Вернуться на главную
+      </Button>
+    </div>
+  );
 };
