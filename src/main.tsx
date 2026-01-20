1719e32ccb2a83836ade2a3482b4e9e237cde34b
tearzday <biverdaniil@gmail.com>
2026-01-20 10:22:59 +0300

    style: add normalize, fonts, variables

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { AppRouter } from './app/providers/RouterProvider/index.ts';
+import './app/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
